c7e2fe8116d16145eb22694914e0e327c1e97726
BB <me@yomama.com>
2023-08-06 22:42:57 +0300

    -

--- news.py
+++ news.py
@@ -6,20 +6,27 @@ import re, time, os
 def strip_html(input):
     return BeautifulSoup(input, "lxml").text
 
-skip_words = ["Turk", "Türkiye", "battery","Webb", "electric","Blinken","Biden","Ocasio",
+skip_words = ["Turk", "Türkiye", "Turkey","battery","Webb", "electric","Blinken","Biden",
               "lithium", "AOC", "der Leyen", "Erdo.an","Elon", "Musk","Tesla",
               "batteries", "SpaceX", "Mars","black hole", "artificial intelligence",
               " AI ", "AI ", " AI", "poll", "Zelensky", "black hole", " EV ", "Webb",
-              "A\.I\.", "telescope", "Yellen", "Francis","Thunberg","tweet","charging",
+              "A\.I\.", "telescope", "Yellen", "Thunberg","charging","Ocasio",
               "tweets","twitter","ChatGPT", "EVs", "electrification", "charger","AI's",
-              "Jon Stewart", "quantum", "power grid", "ronaldo", "Wagner", "Trump",
-              "LGBTQ+", "Cathie Wood", "Guterres", "AI,","Pope"]
+              "Jon Stewart", "quantum", "power grid", "ronaldo", "Wagner", 
+              "LGBTQ+", "Cathie Wood", "Guterres", "AI,","Pope","Andrew Tate"]
 
 
 
 def getnews():
     feeds = [
         ("H2 Central","https://hydrogen-central.com/feed/",20),
+        ("Politico.eu","https://www.politico.eu/feed/",5),
+        ("First Post","https://www.firstpost.com/rss/world.xml",10),
+        ("France 24","https://www.france24.com/en/rss",10),
+        ("Politico","https://www.politico.com/rss/politicopicks.xml",10),
+        ("TDB","https://feeds.thedailybeast.com/summary/rss/articles",10),
+        ("The Atlantic", "https://www.theatlantic.com/feed/all",8),
+        ("Informed Comment","https://www.juancole.com/feed",10),        
         ("Independent, The", "http://www.independent.co.uk/news/world/rss", 10),
         ("The Guardian","http://www.theguardian.com/world/rss",10),
         ('Al Monitor','https://www.al-monitor.com/rss',20),
@@ -27,19 +34,13 @@ def getnews():
         ("Arab News","https://www.arabnews.com/cat/3/rss.xml",10),
         ("Jane's Defence", "https://www.janes.com/feeds/news", 10),
         ("Japan Times","https://www.japantimes.co.jp/feed/",7),
+        ("CGTN","https://www.cgtn.com/subscribe/rss/section/world.xml",10),
         ('CNBC','https://www.cnbc.com/id/100727362/device/rss/rss.html',10),        
         ('WSJ','https://feeds.a.dj.com/rss/RSSOpinion.xml',10),
         ("NYT", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",15),
         ("WION","https://www.wionews.com/feeds/world/rss.xml",20),        
         ("TFI Global","https://tfiglobalnews.com/feed/",20),
-        ("Hindustan Times World","https://www.hindustantimes.com/feeds/rss/world-news/rssfeed.xml",10),
-        ("Politico.eu","https://www.politico.eu/feed/",5),
-        ("First Post","https://www.firstpost.com/rss/world.xml",10),
-        ("France 24","https://www.france24.com/en/rss",10),
-        ("Politico","https://www.politico.com/rss/politicopicks.xml",10),
-        ("TDB","https://feeds.thedailybeast.com/summary/rss/articles",10),
-        ("The Atlantic", "https://www.theatlantic.com/feed/all",8),
-        ("Informed Comment","https://www.juancole.com/feed",10)
+        ("Hindustan Times World","https://www.hindustantimes.com/feeds/rss/world-news/rssfeed.xml",10)
     ]
 
     content = ""
