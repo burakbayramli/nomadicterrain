af58a98953a0c376f0d170d919c1818ea3c79681
BB <me@yomama.com>
2023-10-04 13:17:32 +0300

    -

---
+++ static/weather/weather.js
@@ -0,0 +1,52 @@
+
+
+function fetchForecast() {
+    var endpoint = "https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=";
+    prefs = get_prefs();
+    endpoint = endpoint + prefs['weather']['owm_key']
+    
+    fetch(endpoint)
+	.then(function (response) {
+	    if (200 !== response.status) {
+		console.log(
+		    "Looks like there was a problem. Status Code: " + response.status
+		);
+		return;
+	    }
+
+	    response.json().then(function (data) {
+		console.log(data);
+		var fday = "";
+		data.daily.forEach((value, index) => {
+		    if (index > 0) {
+			var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
+			    weekday: "long",
+			});
+			var temp = value.temp.day.toFixed(0);
+			var hum = value.humidity.toFixed(0);
+			console.log(temp + " " + hum)
+		    }
+		});
+	    });
+	})
+	.catch(function (err) {
+	    console.log("Fetch Error :-S", err);
+	});
+}
+
+function init() {
+    init_cookies(); 
+    prefs = get_prefs();
+    console.log(prefs);
+    if ('owm_key' in prefs['weather']) {
+	document.getElementById("owm_key").value = prefs['weather']['owm_key'];
+    }
+    fetchForecast();
+}
+
+function set_owm_key() {
+    prefs = get_prefs();
+    prefs['weather']['owm_key'] = document.getElementById("owm_key").value;
+    save_cookie(prefs);
+}
+
