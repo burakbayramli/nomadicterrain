30dfc83d296655cfe995f344b3ebf9acfbab6dac
BB <me@yomama.com>
2023-10-06 11:33:11 +0300

    -

--- static/news/tst1.js
+++ static/news/tst1.js
@@ -1,10 +1,14 @@
 
-
 var news_sources = [["Arab News","https://www.arabnews.com/cat/3/rss.xml"]];
+
+var skip_words = ['Trump','South'];
+//var skip_words = [];
 
 function visit() {
     // based on https://github.com/pokiiio/hatena-blog-parser
+    out = "";
     news_sources.forEach(function(elem) {
+	out += `<h3>${elem[0]}</h3>`;
 	console.log(elem[0],elem[1]);
 	url = elem[1];
 	var xmlHttp = new XMLHttpRequest();
@@ -18,12 +22,19 @@ function visit() {
             const postTitle = element.split('<title>')[1].split('</title>')[0];
             const postLink = element.split('<link>')[1].split('</link>')[0];
             const postDescr = element.split('<description>')[1].split('</description>')[0];
-	    console.log(postTitle);
-	    console.log(postLink);
-	    console.log(postDescr);
+	    let skip_word_found = false;
+	    skip_words.forEach(function(word) {
+		if (postDescr.includes(word) || postTitle.includes(word)) {
+		    skip_word_found = true;
+		}
+	    })
+	    if (! skip_word_found) {
+		out += `<p><a href="${postLink}">${postTitle}</a><br/><br/>${postDescr}</p>`;
+	    }
 	});
 
     })
+    document.getElementById("news").innerHTML = out;
 }
 
 function doit() {
