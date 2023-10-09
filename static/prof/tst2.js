fe931e196739bef65887805f3896aa978fc09a6f
BB <me@yomama.com>
2023-10-09 14:47:17 +0300

    -

--- static/prof/tst2.js
+++ static/prof/tst2.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 
 file = fs.readFileSync("chinese.json", 'utf8');
-//file = fs.readFileSync("tst2.json", 'utf8');
 const chinese = JSON.parse(file);
 
 console.log(chinese[10])
+
+file = fs.readFileSync("spiller.json", 'utf8');
+const spiller = JSON.parse(file);
+
+console.log(spiller[10])
 
