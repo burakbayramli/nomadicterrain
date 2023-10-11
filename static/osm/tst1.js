2cc3f74d4442b4b15a4a6e846101f64db2fcde77
BB <me@yomama.com>
2023-10-11 22:20:26 +0300

    -

---
+++ static/osm/tst1.js
@@ -0,0 +1,25 @@
+
+function init()  {
+
+    (async () => {
+	const api = await fetch('https://www.overpass-api.de/api/interpreter?', {
+	    method: 'POST',
+	    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	    },
+	    body:"[out:json];node(48.865,2.25,48.9,2.27)[amenity=restaurant];out;"
+	});
+	const answer = await api.json();
+	console.log(answer);
+
+	out = "";
+
+	answer.elements.forEach(function(elem) {
+	    console.log(elem);
+	})
+	
+	document.getElementById("output").textContent = out;
+    })()
+
+}
