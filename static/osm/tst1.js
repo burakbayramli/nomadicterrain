68993cd41242fbe4c7c7628eebbf6cd500a51a6a
BB <me@yomama.com>
2023-10-12 13:22:42 +0300

    -

--- static/osm/tst1.js
+++ static/osm/tst1.js
@@ -16,12 +16,13 @@ function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     document.getElementById("position").innerHTML = lat + " " + lon;
+    document.getElementById("entry1").style.display = "block";
     map = L.map('map').setView([lat,lon], 12);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: 'ddd'
     }).addTo(map);
-    
+    markers = [];
 }
 
 
@@ -34,7 +35,7 @@ function test1() {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json'
 	    },
-	    body:`[out:json];node['amenity'~'cafe'](around:3000,${lat},${lon});out center;`
+	    body:`[out:json];node['amenity'~'cafe'](around:500,${lat},${lon});out center;`
 	});
 	const answer = await api.json();
 	elems = answer['elements'];
@@ -48,3 +49,38 @@ function test1() {
 }
     
 
+function test2() {
+
+    var type = document.getElementById("amenity_type").value;
+    var name = document.getElementById("amenity_name").value;
+    var dist = parseInt(document.getElementById("amenity_dist").value);
+
+    console.log(name);
+    console.log(type);
+    console.log(dist);
+
+    for(var i = 0; i < this.markers.length; i++){
+	map.removeLayer(this.markers[i]);
+    }
+    (async () => {
+	const api = await fetch('https://www.overpass-api.de/api/interpreter?', {
+	    method: 'POST',
+	    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	    },
+	    body:`[out:json];node['amenity'~'${type}'](around:${dist},${lat},${lon});out center;`
+	});
+	const answer = await api.json();
+	elems = answer['elements'];
+	markers = []
+	elems.forEach(function(x) {
+	    var m = L.marker([x['lat'],x['lon']]).addTo(map);
+	    markers.push(m);
+	    m.bindPopup(x['tags']['name']).openPopup();
+	});
+	
+    })()    
+
+    
+}
