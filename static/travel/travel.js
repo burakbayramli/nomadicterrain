98e5558432bf25ba8877930f122db3d7de2679f9
BB <me@yomama.com>
2023-10-13 19:21:12 +0300

    -

---
+++ static/travel/travel.js
@@ -0,0 +1,9 @@
+
+function init() {
+    url = "https://raw.githubusercontent.com/burakbayramli/kod/master/travel/urla/index.json";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url = url, false ); 
+    xmlHttp.send( null );
+    result = xmlHttp.responseText;
+    console.log(result);
+}
