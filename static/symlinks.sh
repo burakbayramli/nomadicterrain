ae594776dcfded6ae7750c3453d20920534c9f30
BB <me@yomama.com>
2023-10-14 08:29:41 +0300

    -

---
+++ static/symlinks.sh
@@ -0,0 +1,3 @@
+ln -s /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Documents/kitaplar .
+ln -s /home/burak/Documents/repos/burakbayramli.github.com/recom recomdata
+ln -s /home/burak/Documents/kod/travdata .
