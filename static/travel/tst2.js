ad60b244d43b77ac7c061dcd1c65b72495f97e33
BB <me@yomama.com>
2023-10-16 11:22:42 +0300

    -

--- static/travel/tst2.js
+++ static/travel/tst2.js
@@ -5,4 +5,20 @@ file = fs.readFileSync("/home/burak/Documents/repos/nomadicterrain/static/travda
 const re = /<trkseg>[^\0]*?<\/trkseg>/gm;
 const arr = file.match(re);
 console.log(arr.length);
-console.log(arr[0]);
+//console.log(arr[0]);
+
+const re2 = /trkpt lat="\d+\.\d+"\s+lon="\d+\.\d+"/mg;
+const arr2 = arr[0].match(re2);
+console.log(arr2.length);
+//console.log(arr2);
+res = []
+arr2.forEach(function(x) {
+    xx = x.replace('trkpt lat=','');
+    xx = xx.replace('lon=','');
+    xx = xx.replace(/"/g, '');
+    xx = xx.split(' ');
+    res.push([parseFloat(xx[0]), parseFloat(xx[1])]);
+})
+console.log(res);
+
+
