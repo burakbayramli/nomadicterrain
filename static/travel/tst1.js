3ebfbf4a3ef4b4f2fe555da3596d5411651ad9a3
BB <me@yomama.com>
2023-10-17 12:28:26 +0300

    -

--- static/travel/tst1.js
+++
@@ -1,5 +0,0 @@
-url = "http://localhost:5000/static/travdata/polonezkoy/kasdjfk.html";
-
-console.log();
-
-	    
--- static/travel/tst4.js
+++
@@ -1,5 +0,0 @@
-
-url = "http://localhost:5000/static/travdata/polonezkoy/index.json";
-
-path = url.substring(0,url.lastIndexOf("/")+1);
-console.log(path);
