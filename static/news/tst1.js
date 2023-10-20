88f038e16275b0184a73f4622c765360de5fa8da
BB <me@yomama.com>
2023-10-20 11:11:18 +0300

    -

--- static/news/tst1.js
+++ static/news/tst1.js
@@ -1,6 +1,7 @@
 
 function init() {
-    url = "https://corsproxy.io/?https://query1.finance.yahoo.com/v7/finance/download/^IXIC?period1=1689958220&period2=1697734220&interval=1d&events=history&includeAdjustedClose=true";
+    //url = "https://corsproxy.io/?https://query1.finance.yahoo.com/v7/finance/download/^IXIC?period1=1689958220&period2=1697734220&interval=1d&events=history&includeAdjustedClose=true";
+    url = "https://corsproxy.io/?https://fred.stlouisfed.org/graph/fredgraph.csv?id=WALCL";
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url = url, false ); 
     xmlHttp.send( null );
