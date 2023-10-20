5dba94173999a77c057cb3ee9de101b2dca2e881
BB <me@yomama.com>
2023-10-20 12:21:41 +0300

    -

--- static/market/tst1.js
+++ static/market/tst1.js
@@ -1,7 +1,27 @@
 
-var ts = require("./timeseries.js");
+//var ts = require("./timeseries.js");
 
-input = [1.09,1.05,0.94,0.77,1.09,1.21,0.83];
+//input = [1.09,1.05,0.94,0.77,1.09,1.21,0.83];
 
-console.log(ts.cumprod(input));
+//console.log(ts.cumprod(input));
+
+const fs = require('fs')
+
+file = fs.readFileSync("fredout.csv", 'utf8');
+
+//console.log(file);
+
+function parse_fred(file) {
+    lines = file.split('\n');
+    res = [];
+    for (var i=0;i<lines.length;i++) {
+	tokens = lines[i].split(',');
+	res.push(tokens[0],parseFloat(tokens[1]));
+    }
+
+    return res;
+}
+
+r = parse_fred(file);
+console.log(r);
 
