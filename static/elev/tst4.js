41eed0acebd8cab86c4123c7e10681f2777222b8
BB <me@yomama.com>
2023-10-26 13:56:26 +0300

    -

--- static/elev/tst4.js
+++ static/elev/tst4.js
@@ -4,7 +4,7 @@ var K = 3;
 
 function replace_block(mat, block, new_val) {    
     console.log(block[0],block[1]);
-    var k=0
+    var k=0;
     var l=0;
     for (var i=block[0]*K; i<((block[0]+1)*K); i++) {
 	for (var j=block[1]*K; j<((block[1]+1)*K); j++) {
@@ -17,17 +17,19 @@ function replace_block(mat, block, new_val) {
     
 }
 
-//var m = math.zeros([K*3,K*2]);
-//var newm = [[1,2,3],[4,5,7],[8,9,10]];
-//replace_block(m, [0,1], newm);
-//console.log(m);
+var m = math.zeros([K*3,K*2]);
+var newm = [[1,2,3],[4,5,7],[8,9,10]];
+replace_block(m, [0,1], newm);
+console.log(m);
 
 var latbeg = 40;
 var latend = 42;
 var lonbeg = 30;
 var lonend = 31;
+
 var k=0;
 var l=0;
+
 for (var i=latend;i>=latbeg;i--) {    
     for (var j=lonbeg;j<=lonend;j++) {
 	console.log(i,j,k,l)
