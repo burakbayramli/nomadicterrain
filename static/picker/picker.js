8c77a5618f6da4573824dab6aabd80ab536ac35d
BB <me@yomama.com>
2023-11-06 10:58:29 +0300

    -

---
+++ static/picker/picker.js
@@ -0,0 +1,17 @@
+
+function show() {
+
+    map = L.map('map').setView([40,30], 4);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: 'ddd'
+    }).addTo(map);
+
+    map.on('click', function(e){
+	var coord = e.latlng;
+	var latclick = coord.lat;
+	var lngclick = coord.lng;
+	var s = "Picked: " + latclick + " " + lngclick;
+	document.getElementById('picked').innerHTML = s;
+    });    
+}
