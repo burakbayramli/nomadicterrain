e7fe7b72809fa4a68bf233657d265a892a962a35
BB <me@yomama.com>
2023-11-10 10:47:26 +0300

    -

--- static/elev/elev.js
+++ static/elev/elev.js
@@ -15,47 +15,39 @@ function in_tens(x) {
     return parseInt(parseInt(parseFloat(parseInt(x)) / 10)*10);
 }
 
-function add_elev(latint, lonint, arrx, arry, arrz) {
+function plot_elevation () {
 
-    console.log('in add elev',latint,lonint)
-    tenslat = in_tens (lat);
-    tenslon = in_tens (lon);
+    latint = parseInt(lat);
+    lonint = parseInt(lon);
 
-    url = `/static/elev/data/out-${tenslat}-${tenslon}.json`;
+    tenslat = in_tens (latint);
+    tenslon = in_tens (lonint);
+
+    console.log(latint,lonint,tenslat,tenslon);
+    
+    url = `/static/elev/data/out-${tenslat}-${tenslon}.json`;    
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url = url, false ); 
     xmlHttp.send( null );
     result = xmlHttp.responseText;
+
     res = JSON.parse(result);
 
     W = 1/120
+    x = [];
+    y = [];
     for (var j=latint+1;j>latint+W;j-=W) {
-	arry.push (j);
+	y.push (j);
     }
     for (var i=lonint;i<lonint+1-W;i+=W) {
-	arrx.push (i);
+	x.push (i);
     }    
+
     var key = `${latint}-${lonint}`;
-    res[key].forEach(function(z) {
-	arrz.push(z);
-    });
-    console.log(arrx.length, arry.length, arrz.length);
-}
-
-function plot_elevation () {
-
-    lat=40.5; lon=29.5;
-    var radius = 50;
-    
-    var tmpx = []; var tmpy = []; var tmpz = [];
-    add_elev(42,29,tmpx,tmpy,tmpz);
-    add_elev(41,29,tmpx,tmpy,tmpz);
-    add_elev(40,29,tmpx,tmpy,tmpz);
-    
     var data = [ {
-	x: tmpx,
-	y: tmpy,
-	z: tmpz,
+	x: x,
+	y: y,
+	z: res[key],
 	colorscale: "Earth",
 	type: 'contour',
 	showlabels: true,
@@ -68,25 +60,23 @@ function plot_elevation () {
 	} 
     } ];
 
-    //var radius = parseInt(document.getElementById("radius").value);
-    //console.log(radius);
-    //console.log(lat);
-    //console.log(lon);
+    var radius = parseInt(document.getElementById("radius").value);
+    console.log(radius);
+    console.log(lat);
+    console.log(lon);
     var S = 20;
     var xmin = lon - (radius / S);
     var xmax = lon + (radius / S);
     var ymin = lat - (radius / S);
     var ymax = lat + (radius / S);
-    //console.log(xmin,xmax,ymin,ymax);
+    console.log(xmin,xmax,ymin,ymax);
     
     var layout = {
 	title: 'Basic Contour Plot',
 	xaxis: {
-	    //range: [29.0, 29.5]  
 	    range: [xmin, xmax]  
 	},
 	yaxis: {
-	    //range: [40.92, 40.98]  
 	    range: [ymin, ymax]  
 	}	
     }
