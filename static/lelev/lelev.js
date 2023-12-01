69c90e32596d6472618c5230ded200957d8c563a
BB <me@yomama.com>
2023-12-01 10:27:22 +0300

    -

--- static/lelev/lelev.js
+++ static/lelev/lelev.js
@@ -1,4 +1,25 @@
 
+KM_DEG = 0.009;
+
+function degToRad(deg) {
+    return deg * (Math.PI / 180.0);
+}
+
+function goto(fr, a, d) {
+
+    var N = 30;
+    var D = d*KM_DEG;
+    var res = [];
+    var P  = D / N
+    //console.log(Math.sin(degToRad(180)));
+    //console.log(Math.sin(2*Math.PI / 4));
+    for (var i=0;i<N; i++) {
+	lat = fr[0] + (P*i)*Math.sin(a);
+	lon = fr[1] + (P*i)*Math.cos(a);
+	res.push([lat,lon]);
+    }
+    return res;
+}
 
 function init() {
 
