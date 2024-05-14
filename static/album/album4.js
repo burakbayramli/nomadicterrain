69fcb93b194914dbe8ea1434db3aaeaa56233fe4
BB <me@yomama.com>
2024-05-14 11:22:35 +0300

    -

--- static/album/album4.js
+++ static/album/album4.js
@@ -46,7 +46,7 @@ function showpageparam() {
     var myParam = location.search.split('photo=')[1]    
     console.log(myParam);
     var out = "";
-    out += "<div>" + album['photos'][myParam]['desc'] + "</div>"; 
+    out += "<div><center>" + album['photos'][myParam]['desc'] + "</center></div>"; 
     out += "<img onclick='xy_click(event)' width='500' src='" + album['photos'][myParam]['url'] + "'></img>";
     document.getElementById('output').innerHTML = out;		  
     
