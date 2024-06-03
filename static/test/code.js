0fdb6d3889574774f72f40af94246bba0ccc04b4
BB <me@yomama.com>
2024-06-03 12:55:47 +0300

    -

---
+++ static/test/code.js
@@ -0,0 +1,18 @@
+
+function init() {
+
+    //var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
+    var url = "http://192.168.43.49:5000/static/in.zip";
+
+    fetch(url).then(res => res.arrayBuffer()).then(arrayBuffer => {
+	var new_zip = new JSZip();
+	new_zip.loadAsync(arrayBuffer)
+	.then(function(zip) {
+	    //var res = zip.file(`20240602.export.CSV`).async('string');
+	    var res = zip.file('in.csv').async('string');
+	});
+    }).then(function(done) {
+	console.log('done');
+    });
+    
+}
