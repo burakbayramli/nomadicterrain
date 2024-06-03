5a5e26fa3104711627306d691588d6adc35add41
BB <me@yomama.com>
2024-06-03 15:01:31 +0300

    -

--- static/test/code1.js
+++ static/test/code1.js
@@ -1,6 +1,4 @@
 
-// cols = ['GlobalEventID', 'Day', 'MonthYear', 'Year', 'FractionDate','Actor1Code', 'Actor1Name', 'Actor1CountryCode','Actor1KnownGroupCode', 'Actor1EthnicCode' 'Actor1Religion1Code', 'Actor1Religion2Code', 'Actor1Type1Code', 'Actor1Type2Code', 'Actor1Type3Code', 'Actor2Code', 'Actor2Name', 'Actor2CountryCode', 'Actor2KnownGroupCode', 'Actor2EthnicCode', 'Actor2Religion1Code', 'Actor2Religion2Code', 'Actor2Type1Code', 'Actor2Type2Code', 'Actor2Type3Code', 'IsRootEvent','EventCode', 'EventBaseCode']
-
 function init() {
 
     //var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
