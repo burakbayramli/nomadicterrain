f4b1188a1d0550cdd953f781427cd1ae5a914b0e
BB <me@yomama.com>
2024-06-04 11:37:14 +0300

    -

--- static/test/code4.js
+++ static/test/code4.js
@@ -21,16 +21,18 @@ function init() {
 
     display_map();
     
-    var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
+    //var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
+    //var url = "http://192.168.43.49:5000/static/20240525.export.CSV.zip";
+    var url = "http://192.168.43.49:5000/static/20240526.export.CSV.zip";
     //var url = "http://data.gdeltproject.org/events/20240602.export.CSV.zip";
-    //var url = "http://192.168.43.49:5000/static/in2.zip";
 
     fetch(url).then(res => res.arrayBuffer()).then(arrayBuffer => {
 	var new_zip = new JSZip();
 	new_zip.loadAsync(arrayBuffer)
 	.then(function(zip) {
-	    var res = zip.file(`20240602.export.CSV`).async('string');
-	    //var res = zip.file('in2.csv').async('string');
+	    //var res = zip.file(`20240602.export.CSV`).async('string');
+	    //var res = zip.file(`20240525.export.CSV`).async('string');
+	    var res = zip.file(`20240526.export.CSV`).async('string');
 	    return res;
 	}).then(function(text) {
 	    //console.log(text);
