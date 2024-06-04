596894f1cb86d3053b9acd4aed0d55914ca04bdf
BB <me@yomama.com>
2024-06-04 10:16:45 +0300

    -

--- static/test/code3.js
+++ static/test/code3.js
@@ -3,25 +3,27 @@ var cols = ["GLOBALEVENTID", "SQLDATE", "MonthYear", "Year", "FractionDate", "Ac
 
 function init() {
 
-    //var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
-    //var url = "http://192.168.43.49:5000/static/in.zip";
-    var url = "http://192.168.43.49:5000/static/in2.zip";
+    var url = "http://192.168.43.49:5000/static/20240602.export.CSV.zip";
+    //var url = "http://data.gdeltproject.org/events/20240602.export.CSV.zip";
+    //var url = "http://192.168.43.49:5000/static/in2.zip";
 
     fetch(url).then(res => res.arrayBuffer()).then(arrayBuffer => {
 	var new_zip = new JSZip();
 	new_zip.loadAsync(arrayBuffer)
 	.then(function(zip) {
-	    //var res = zip.file(`20240602.export.CSV`).async('string');
-	    var res = zip.file('in2.csv').async('string');
+	    var res = zip.file(`20240602.export.CSV`).async('string');
+	    //var res = zip.file('in2.csv').async('string');
 	    return res;
 	}).then(function(text) {
 	    //console.log(text);
 	    var lines = text.split('\n');
 	    lines.forEach(function(line) {
-		console.log('line', line);
+		//console.log('line', line);
 		var tokens = line.split('\t');
-		console.log(tokens.length); 
+		//console.log(tokens.length); 
 	    });
+	}).then(function(done) {
+	    console.log('done');
 	});
     });
 }
--- static/test/readme.md
+++ static/test/readme.md
@@ -1,6 +1,9 @@
 
-zip
-  http://data.gdeltproject.org/events
-  https://stuk.github.io/jszip/documentation/examples.html
-  http://192.168.43.49:5000/static/test/t1.html
+http://data.gdeltproject.org/events
+
+http://data.gdeltproject.org/events/20240603.export.CSV.zip
+
+https://stuk.github.io/jszip/documentation/examples.html
+
+http://192.168.43.49:5000/static/test/t1.html
 
