0a920d71bc317eb19c6c47189a29417c2aaa2c77
BB <me@yomama.com>
2024-06-06 12:45:39 +0300

    -

--- static/path/path.js
+++ static/path/path.js
@@ -57,14 +57,14 @@ function setCurrentToCallback(position) {
 }
 
 function setPickerFrom() {
-    coords = prefs['picker']['coord'].split(" ");
+    coords = sessionStorage.getItem('picker_coord').split(" ");
     lat = parseFloat(coords[0]);
     lon = parseFloat(coords[1]);
     document.getElementById("coordfr").value = lat + "," + lon;    
 }
 
 function setPickerTo() {
-    coords = prefs['picker']['coord'].split(" ");
+    coords = sessionStorage.getItem('picker_coord').split(" ");
     lat = parseFloat(coords[0]);
     lon = parseFloat(coords[1]);
     document.getElementById("coordto").value = lat + "," + lon;    
