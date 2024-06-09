ff3228cf235909bc59de13ae41e012c21cf9b38f
BB <me@yomama.com>
2024-06-09 12:45:19 +0300

    -

--- static/main.js
+++ static/main.js
@@ -1,6 +1,26 @@
 
-
 function init() {
-    console.log("----");
-    //document.getElementById('reminder').innerHTML = "asdfkalsdf";
+    var rt = localStorage.getItem('reminder_text');
+    const date = new Date();
+    const d1 = date.getDate();
+    const m1 = date.getMonth() + 1;
+    const y1 = date.getFullYear();    
+    console.log(rt);
+    console.log(d1, m1, y1);
+    if (rt != null) {
+	rt = JSON.parse(rt);
+	Object.keys(rt).forEach(function (x) {
+	    console.log('------------------------');
+	    console.log(x, rt[x]);
+	    var [d2,m2,y2] = x.split("/");
+	    console.log(d2,m2,y2);
+	    console.log(typeof(d1));
+	    console.log(typeof(d2));
+	    if ( (d1==d2 | d2=="*") & (m1==m2|m2=="*") & (y1==y2 | y2=="*") ) {
+		console.log("match");
+	    }
+	});
+    }
+    
 }
+
--- static/reminders/reminders.js
+++ static/reminders/reminders.js
@@ -1,4 +1,12 @@
 
-function save() {
-    console.log(document.getElementById('reminder_input').value);
+function init() {
+    var rt = localStorage.getItem("reminder_text");
+    console.log(rt);
+    document.getElementById('reminder_input').innerText =  rt;
 }
+
+function save() {
+    var rt = document.getElementById('reminder_input').value;
+    localStorage.setItem("reminder_text", rt);
+}
+
