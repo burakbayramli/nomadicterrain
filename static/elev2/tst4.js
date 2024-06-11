7e5625fd5fb98e326de14dc36e93263ee2321c50
BB <me@yomama.com>
2024-06-11 12:37:26 +0300

    -

---
+++ static/elev2/tst4.js
@@ -0,0 +1,89 @@
+'use strict';
+//var fs= require('fs');
+var resolution= 120;
+
+var dataFiles= [
+    // ...
+    { name: 'g10g', latMin:     0, latMax:     50, lngMin:      0, lngMax:     90, elMin:   -407, elMax:    8752, columns:    10800, rows:   6000 }
+    // ...
+];
+
+var baseDir = './all10';
+
+var byteArray;
+
+function load() {
+    var url = "/static/elev2/all10/g10g";    
+    fetch(url).then(res => res.arrayBuffer())
+	.then(arrayBuffer => {
+	    byteArray = new Uint8Array(arrayBuffer);
+	})
+	.then(function(done) {
+	    console.log('done');
+	    //console.log(byteArray);
+	    console.log(byteArray[33681360]);
+	    console.log(byteArray[33681361]);
+
+	    var buffer = new ArrayBuffer(2);
+	    var Uint8View = new Uint8Array(buffer);
+	    Uint8View[0] = byteArray[33681360];
+	    Uint8View[1] = byteArray[33681361]
+
+	    var Uint16View = new Uint16Array(buffer);
+	    console.log(Uint16View[0]);	    
+	})
+	.catch(error => {
+	    console.log('error');	    
+        });
+}
+
+function show() {
+    console.log(byteArray);
+}
+
+function findFile( lng, lat ) {
+    for ( var i in dataFiles ) {
+        var df= dataFiles[i];
+        if (df.latMin <= lat && df.latMax > lat && df.lngMin <= lng && df.lngMax > lng) {
+            return df;
+        }
+    }
+}
+
+function fileIndex( lng, lat, fileEntry, resolution ) {
+    var column= Math.floor(lng * resolution);
+    var row= Math.floor(lat * resolution);
+
+    var rowIndex= row - fileEntry.latMin * resolution;
+    var columnIndex= column - fileEntry.lngMin * resolution;
+    var index= ((fileEntry.rows - rowIndex - 1) * fileEntry.columns + columnIndex) * 2;
+    return index;
+};
+
+function openFile( name ) {
+    return fs.openSync(baseDir + '/' + name , 'r');
+}
+
+function readNumberFromFile(name,position) {
+
+    var buffer= new Buffer(2);
+    var fd = openFile(name);
+    // fs.readSync(fd, buffer, offset, length[, position])
+    console.log('pos',position);
+    if ( fs.readSync(fd, buffer, 0, 2, position) !== 2 ) return new Error('Could not fetch value from file');
+
+    var int16= buffer.readInt16LE(0);
+    
+    // return 0 for oceans
+    return int16 === -500 ? 0 : int16;
+}
+
+function getElevation( lng, lat, onError ) {
+    var fileEntry= findFile(lng, lat);
+    var result= readNumberFromFile(fileEntry.name, fileIndex(lng, lat, fileEntry, resolution));
+
+    return result;
+};
+
+//var res = getElevation(29,37);
+//console.log(res);
