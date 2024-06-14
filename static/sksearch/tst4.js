74c0a72196eb33036f2f821813296ed42645071a
BB <me@yomama.com>
2024-06-14 14:34:23 +0300

    -

--- static/sksearch/tst4.js
+++ static/sksearch/tst4.js
@@ -1,18 +1,42 @@
 
 async function searchText() {
 
-    var url = "/static/skidx/invidx-o.json";
+    var stoks = ["oturum","baglanti"];
 
-    var stoks = ["oturum"];
-
+    var stok_hits = {}
+    
     for (var i=0; i<stoks.length; i++) {
 	var tok = stoks[i];
-	console.log('-------------------',tok);
-	var letter_dict;    
+	var letter_dict;
+	var firstLetter = tok.substring(0,1);
+	var url = `/static/skidx/invidx-${firstLetter}.json`;
 	await fetch(url)
 	    .then(response => response.json())
 	    .then(data => letter_dict = data );
 	
-	console.log(letter_dict['oturum']);
+	if (letter_dict.hasOwnProperty(tok)) {
+	    console.log('has',tok);
+	    Object.keys(letter_dict[tok]).forEach(function(article) {
+		if (stok_hits.hasOwnProperty(article)) {
+		    stok_hits[article] = stok_hits[article] + letter_dict[tok][article];
+		} else {
+		    stok_hits[article] = letter_dict[tok][article];
+		}
+	    });	    
+	}	
+    }
+
+    var keyValues = []
+
+    for (var key in stok_hits) {
+	keyValues.push([ key, stok_hits[key] ])
+    }
+
+    keyValues.sort(function compare(kv1, kv2) {
+	return kv2[1] - kv1[1] 
+    })
+
+    for (var i=0;i<10;i++) {
+	console.log(keyValues[i]);
     }
 }
