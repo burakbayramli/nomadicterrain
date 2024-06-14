0a32bfe58837d13c24a9bbfe45ae3ddd431addff
BB <me@yomama.com>
2024-06-14 11:10:36 +0300

    -

--- static/symlinks.sh
+++ static/symlinks.sh
@@ -1,3 +1,4 @@
 ln -s /mnt/3d1ece2f-6539-411b-bac2-589d57201626/home/burak/Documents/kitaplar .
 ln -s /home/burak/Documents/repos/burakbayramli.github.com/recom recomdata
 ln -s /home/burak/Documents/kod/travdata .
+ln -s /opt/Downloads/alldata/skidx .
