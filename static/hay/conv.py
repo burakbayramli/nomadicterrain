914fa8195b2abe9918e35fa0883817b2ddb35df8
BB <me@yomama.com>
2024-07-11 08:55:42 +0300

    -

---
+++ static/hay/conv.py
@@ -0,0 +1,18 @@
+# Converts txt to json
+import re, json
+
+res = open("hay.txt").read()
+
+res = res.split("\n\n")
+
+d = {}
+
+for x in res:
+    if ":" in x:
+        x = x.strip()
+        a,b = x.split(":")
+        d[a] = b
+        
+fout = open("hay.json","w")
+fout.write(json.dumps(d))
+fout.close()
