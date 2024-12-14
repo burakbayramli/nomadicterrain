163cbab78065bbb7720a17de89dd2b80a195002a
BB <me@yomama.com>
2024-12-14 18:08:56 +0300

    -

--- guide/jlewi/run.sh
+++
@@ -1,4 +0,0 @@
-#javac -classpath src/java/:lib/commons-lang3-3.13.0.jar src/java/org/jlewi/GenerateDecans.java
-#java -classpath src/java/:lib/commons-lang3-3.13.0.jar org.jlewi.GenerateDecans
-javac -classpath src/java/:lib/commons-lang3-3.13.0.jar src/java/swisseph/VedicTest.java
-java -classpath src/java/:lib/commons-lang3-3.13.0.jar swisseph.VedicTest
