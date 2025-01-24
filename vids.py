e72cd857a3c13617f87d9abdbd4d7b8af904856f
BB <me@yomama.com>
2025-01-24 21:02:18 +0300

    -

--- vids.py
+++ vids.py
@@ -6,26 +6,26 @@ skip_words = ["empire","deep state","wolff"]
 
 def getvids(daysago):
     vbase = "https://www.youtube.com/feeds/videos.xml?channel_id="
-    feeds = [
-        ("daniel davis","UCWDN5zr5ttctoIAhZwW6tcQ",5),
-        ("hedges","UCEATT6H3U5lu20eKPuHVN8A",5),
-        ("owen","UCSYCo8uRGF39qDCxF870K5Q",5),
-        ("ameinavan","UCMoXBV0j3BZAMKqm42vpYtg",5),
-        ("clysdale","UCqpNHx_gjAV70G-H1jZMpOg",5),
-        ("alice","UCrCTC5_t-HaVJ025DbYITiw",5),
-        ("niki","UCNmv0SUwrOkEqadKneNRwHg",5),
-        ("ssoberry","UCSZQK43hJ422otti5D3iNgA",5),
-        ("tibees","UC52kszkc08-acFOuogFl5jw",5),
-        ("dialogue works","UCkF-6h_Zgf9zXNUmUB-MzTw",5),
-        ("status coup","UC0pCsHlEEmCfxllZSlRB2Og",5),
-        ("neutrality","UCHdLVKdAeG6zAeZMGZh91bg",5),
-        ("haiph","UCOxLhz6B_elvLflntSEfnzA",5),
-        ("patry","UCzQ5JiuXVX3enojkU3Plalw",5),
-        ("rosen","UCvsIlofcT7DF3Kk8pz9yFfw",5),
-        ("encamp","UCRIeMHsEdzA9RroG19kXdYg",5),
-        ("bouris","UCPX01K1JgNS5pyzUtwtCtsQ",5),
-        ("LPPFusion","UCiBditpj7sdROMYz02qoCMQ",5),
-        ("sky scholar","UCL7QIOZteWPpBWBOl8i0e-g",5)
+    feeds = [ ("daniel davis","UCWDN5zr5ttctoIAhZwW6tcQ",5),
+              ("hedges","UCEATT6H3U5lu20eKPuHVN8A",5),
+              ("owen","UCSYCo8uRGF39qDCxF870K5Q",5),
+              ("ameinavan","UCMoXBV0j3BZAMKqm42vpYtg",5),
+              ("clysdale","UCqpNHx_gjAV70G-H1jZMpOg",5),
+              ("alice","UCrCTC5_t-HaVJ025DbYITiw",5),
+              ("niki","UCNmv0SUwrOkEqadKneNRwHg",5),
+              ("unicamp","UCs0T96Tx2oPuGjRXs7h8OAA",5),                                                       
+              ("tibees","UC52kszkc08-acFOuogFl5jw",5),
+              ("dialogue works","UCkF-6h_Zgf9zXNUmUB-MzTw",5),
+              ("status coup","UC0pCsHlEEmCfxllZSlRB2Og",5),
+              ("neutrality","UCHdLVKdAeG6zAeZMGZh91bg",5),
+              ("haiph","UCOxLhz6B_elvLflntSEfnzA",5),
+              ("patry","UCzQ5JiuXVX3enojkU3Plalw",5),
+              ("rosen","UCvsIlofcT7DF3Kk8pz9yFfw",5),
+              ("encamp","UCRIeMHsEdzA9RroG19kXdYg",5),
+              ("bouris","UCPX01K1JgNS5pyzUtwtCtsQ",5),
+              ("LPPFusion","UCiBditpj7sdROMYz02qoCMQ",5),
+              ("sky scholar","UCL7QIOZteWPpBWBOl8i0e-g",5),
+              ("enkamp","UCRIeMHsEdzA9RroG19kXdYg",5)
     ]
 
     content = ""
