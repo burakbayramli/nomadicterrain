78bc05cdb6994746095d114047d3325cbc86a843
BB <me@yomama.com>
2025-03-10 10:57:41 +0300

    -

---
+++ static/syrmap/syrmap.js
@@ -0,0 +1,38 @@
+
+function plot_map() {
+    map = L.map('map').setView([35,38], 6);
+    
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
+	maxZoom: 19, attribution: 'OSM'
+    }).addTo(map);
+
+    var b = document.getElementById("before").value;
+    var a = document.getElementById("after").value;
+    console.log(a,b);
+    plot(b,a);
+}    
+
+function plot(before,after) {
+
+    console.log(before);
+    console.log(after);
+    
+    url = `/static/syrmap/data/${before}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url = url, false ); 
+    xmlHttp.send( null );
+    result = xmlHttp.responseText;
+    blocks = JSON.parse(result);
+    hts = blocks["HTS"];
+    bef = []
+    hts.forEach(function(x) {
+	bef.push([x[1],x[0]]);
+    });
+    var linebef = new L.Polyline(bef, {
+	color: 'darkblue', weight: 2, dashArray: '3, 3', dashOffset: '0'
+    });
+    linebef.addTo(map);
+    
+
+       
+}
