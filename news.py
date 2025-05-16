2bcd728c9b743911d78f580d35d09da764a2858a
BB <me@yomama.com>
2025-05-16 07:27:56 +0300

    -

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ import re, time, os
 def strip_html(input):
     return BeautifulSoup(input, "lxml").text
 
-skip_words = ["Turkey", "Elon", "Musk","Tesla", "Mars","black hole","AI", "EV"]
+skip_words = ["Pope Leo","Turkey", "Elon", "Musk","Tesla", "Mars"]
 
 def getnews():
     feeds = [
@@ -17,7 +17,18 @@ def getnews():
         ("Hindustan Times World","https://www.hindustantimes.com/feeds/rss/world-news/rssfeed.xml",10),
         ("Informed Comment","https://www.juancole.com/feed",10),
         ("H2 GN","https://news.google.com/rss/search?q=intitle:hydrogen+when:24h&hl=en-US&gl=US&ceid=US:en",5),
-        ("The American Conservative","https://www.theamericanconservative.com/feed",10)
+        ("The American Conservative","https://www.theamericanconservative.com/feed",10),
+        ("Politico","https://www.politico.com/rss/politicopicks.xml",5),
+	("TDB","https://www.thedailybeast.com/arc/outboundfeeds/rss/articles",5),
+	("The Guardian","http://www.theguardian.com/world/rss",5),
+	("CNBC","https://www.cnbc.com/id/100727362/device/rss/rss.html",5),
+	("NYT", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",5),
+	("France 24","https://www.france24.com/en/rss",5),
+	("Al Monitor", "https://www.al-monitor.com/rss",5),
+	("TASS", "http://tass.com/rss/v2.xml",5),
+	("First Post","https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml",5),
+	("WION","https://www.wionews.com/feeds/world/rss.xml",5),
+	("Arab News","https://www.arabnews.com/cat/3/rss.xml",5)
     ]
 
     content = ""
