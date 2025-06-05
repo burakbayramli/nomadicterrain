fa02a06ed30122eed226c9b75d9588cecf4ea47d
BB <me@yomama.com>
2025-06-05 12:02:52 +0300

    -

--- static/vedic/vedic.js
+++ static/vedic/vedic.js
@@ -25,9 +25,10 @@ function calculate() {
 	  links += "<p>North Node</p>";
 	  var north = res['true Node'][0].toLowerCase();
 	  var north2 = capitalizeFirstLetter(north);
+	  var south = res['Ketu (true)'][0].toLowerCase();
 	  links += `<a target="_blank" href="https://vedicsiddhanta.in/2023/09/north-node-in-astrology.html#North_Node_or_Rahu_in_${north2}">NN 1</a>,`;
 	  links += `<a target="_blank" href="https://advanced-astrology.com/north-node-in-${north}/">NN 2</a>,`;
-	  links += `<a target="_blank" href="https://astrostyle.com/astrology/gemini-north-node-${north}-north-node/">NN 3</a>`;
+	  links += `<a target="_blank" href="https://astrostyle.com/astrology/${south}-north-node-${north}-north-node/">NN 3</a>`;
 	  document.getElementById("links").innerHTML = links;
         }
     } 
