e775a8040e8aceb7dc8494446f66f386a115787f
BB <me@yomama.com>
2025-08-26 18:16:16 +0300

    -

--- main.py
+++ main.py
@@ -326,7 +326,7 @@ if __name__ == '__main__':
     app.debug = True
     app.secret_key = "aksdfkasf"
     print (len(sys.argv))
-    elif len(sys.argv) == 2 and sys.argv[1]=="acer":
+    if len(sys.argv) == 2 and sys.argv[1]=="acer":
         app.run(host="192.168.246.102",port=5000)        
     elif len(sys.argv) == 2 and sys.argv[1]=="tableta9":
         app.run(host="192.168.246.193",port=5000)        
