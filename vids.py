fd4830f22339f4ac8f8ad45f90a85c02959588fd
BB <me@yomama.com>
2025-12-05 20:21:24 +0300

    -

--- vids.py
+++ vids.py
@@ -8,22 +8,20 @@ def getvids(daysago):
     vbase = "https://www.youtube.com/feeds/videos.xml?channel_id="
     feeds = [ ("neutrality","UCHdLVKdAeG6zAeZMGZh91bg",5),
               ("clysdale","UCqpNHx_gjAV70G-H1jZMpOg",5),
-              #("alice","UCrCTC5_t-HaVJ025DbYITiw",5),
+              ("alice","UCrCTC5_t-HaVJ025DbYITiw",5),
               ("scottish","UCzCi3Vxl5RPlHZtKJDo2Y9Q",5),
               ("cappy","UCwrpo8z3IgJln2ZidqBUiMA",5),
               ("niki","UCNmv0SUwrOkEqadKneNRwHg",5),
-              ("unicamp","UCs0T96Tx2oPuGjRXs7h8OAA",5),
+              ("em&joel","UCbn5xzRrAOY9DWJlbyPBsvg",5),
               ("werleman","UCa1_vNx2yAQzFnQWNajUHtg",5),
               ("willem","UCF1fG3gT44nGTPU2sVLoFWg",5),
               ("tibees","UC52kszkc08-acFOuogFl5jw",5),              
               ("haiph","UCOxLhz6B_elvLflntSEfnzA",5),
               ("rosen","UCvsIlofcT7DF3Kk8pz9yFfw",5),
               ("encamp","UCRIeMHsEdzA9RroG19kXdYg",5),
-              ("bouris","UCPX01K1JgNS5pyzUtwtCtsQ",5),
               ("LPPFusion","UCiBditpj7sdROMYz02qoCMQ",5),
               ("sky scholar","UCL7QIOZteWPpBWBOl8i0e-g",5),
-              ("dialogue works","UCkF-6h_Zgf9zXNUmUB-MzTw",5),
-              ("status coup","UC0pCsHlEEmCfxllZSlRB2Og",5),              
+              ("dialogue works","UCkF-6h_Zgf9zXNUmUB-MzTw",5)
     ]
 
     content = ""
